513439978901bd6b494ee3487f6a193619047441
Jeffrey Ilar <jilar@petzent.com>
2019-09-26 14:44:26 -0700

    Learned: How to create constructor to class component, state property, conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//class Component always extends React.Component
 class App extends React.Component{
-  render() {
+  //construction class particular to javascript, called when instance of app component is created
+  constructor(props){
+    //required, constructor function from React.component
+    super(props);
+    //ONLY TIME to do direct assignment
+    //to this.state
+    this.state={lat: null, errorMessage:''};
+
+    //get geolocation
     window.navigator.geolocation.getCurrentPosition(
       //success callback function
-      (position) => console.log(position),
+      (position) => {
+        //update state with setState
+        this.setState({lat:position.coords.latitude});
+      },
       //failure callback funtion
-      (err) => console.log(err)
+      (err) => {
+        this.setState({errorMessage:err.message})
+      }
     );
 
-    return <div> Latitude: </div>;
+  }
+
+  //class component must always have a render() method
+  //dont initialize anything in render method,
+  render() {
+      if(this.state.errorMessage && !this.state.lat){
+        return <div>Error: {this.state.errorMessage}</div>;
+      } else if(!this.state.ErrorMessage && this.state.lat) {
+        return <div>Latitude: {this.state.lat}</div>;
+      }
+        return <div>Loading</div>;
   }
 }
 
