5e984ca37d6a5d082822ca13375730d9c02a5d1a
Jeffrey Ilar <jilar@petzent.com>
2019-09-30 11:45:40 -0700

    Refactored geolocation api call from constructor to componentDidMount. Removed  constructor method.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const SeasonDisplay = () =>{
-  <div>Season Display</div>;
+const getSeason = (lat, month) => {
+    if(month <=8 && month >= 3){
+      return lat > 0 ? 'summer' :'winter';
+    }
+    return lat < 0 ? 'winter' :'summer';
+}
+
+const SeasonDisplay = (props) =>{
+  const season =getSeason(props.lat, new Date().getMonth());
+  const icon= season === 'winter' ? 'snowflake' : 'sun';
+  const text= season === 'winter' ? 'Burr, it is chilly!' : 'Lets hit the beach!';
+  return (
+    <div>
+    <i className= {`${icon} icon`} />
+    <h1>{text}</h1>
+    <i className= {`${icon} icon`} />
+    </div>
+  );
 };
 
-export defualt SeasonDisplay;
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay"
 
 //class Component always extends React.Component
 class App extends React.Component{
-  //construction class particular to javascript, called when instance of app component is created
-  constructor(props){
-    //required, constructor function from React.component
-    super(props);
-    //ONLY TIME to do direct assignment
-    //to this.state
-    this.state={lat: null, errorMessage:''};
-
+  // //construction class particular to javascript, called when instance of app component is created
+  // constructor(props){
+  // //required, constructor function from React.component
+  //   super(props);
+  // //ONLY TIME to do direct assignment
+  // //to this.state
+  //   this.state={lat: null, errorMessage:''};
+  // }
+  state={lat: null, errorMessage:''};
+  //happens upon first render, place initial data loading here
+  componentDidMount(){
     //get geolocation
     window.navigator.geolocation.getCurrentPosition(
       //success callback function
@@ -23,6 +27,9 @@ class App extends React.Component{
         this.setState({errorMessage:err.message})
       }
     );
+  }
+  //happens with each class update; occurs AFTER render() is invoked
+  componentDidUpdate(){
 
   }
 
@@ -32,7 +39,7 @@ class App extends React.Component{
       if(this.state.errorMessage && !this.state.lat){
         return <div>Error: {this.state.errorMessage}</div>;
       } else if(!this.state.ErrorMessage && this.state.lat) {
-        return <div>Latitude: {this.state.lat}</div>;
+        return <SeasonDisplay lat={this.state.lat}/>;
       }
         return <div>Loading</div>;
   }
